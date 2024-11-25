5bc1291dbbe4477a495e427ea43c3b7adcbc774b
Muhamad Iksan <muhamadiksan351@gmail.com>
2024-11-25 18:19:01 +0700

    feat: delete routing test

--- routes/web.php
+++ routes/web.php
@@ -14,15 +14,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/user', function () {
-    return "Hallo user!";
-})->role('user');
-
-Route::get('/admin', function () {
-    return "Hallo admin!";
-})->role('admin');
-
-
 Route::get('/', function () {
     return view('welcome');
 });
